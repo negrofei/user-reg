b9f50002224540f13cb57553806b48a51921591f
Martin Feijoo <feijoomartin@gmail.com>
2024-02-19 16:57:58 -0300

    added credentials

--- back/database.py
+++ back/database.py
@@ -10,12 +10,12 @@ def engine_to_database() -> Engine:
     Armamos el objeto engine que se comunica con la base de datos
     """
     # TODO: Mejorar el tema de las credenciales.
-    from . import credentials 
-    user = credentials.user
-    host = credentials.host
-    db = credentials.db
-    pwd = credentials.pwd
-    engine = create_async_engine('mysql://{user}:{pwd}@{host}/{db}'.format(user=user, pwd=pwd, host=host, db=db))
+    from credentials import user,  host, db, pwd
+    # user = credentials.user
+    # host = credentials.host
+    # db = credentials.db
+    # pwd = credentials.pwd
+    engine = create_async_engine('mysql+aiomysql://{user}:{pwd}@{host}/{db}'.format(user=user, pwd=pwd, host=host, db=db))
     return engine
 
 engine = engine_to_database()
